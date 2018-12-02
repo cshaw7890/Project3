3366088f177e9c5d65f186854c927bdced7d39c7
jasonwu0601 <45521499+jasonwu0601@users.noreply.github.com>
2018-12-02 15:34:50 -0500

    Update Truck.java

--- Truck.java
+++ Truck.java
@@ -8,4 +8,31 @@ public class Truck
 {
     City    current_city;
     int     capacity = 500;
+    
+    int     number;
+    int     distance_travelled;
+    
+    public Truck(int n)
+    {
+        current_city = graph.getCity(center);
+        //capacity is 500
+        number = n;
+        distance_travelled = 0;
+    }
+    
+    public void proceed()
+    {
+        City c = this.current_city;
+        
+        //Current place does not have anymore appropriate cargo order done
+        for(int i = 0; i < c.names.size(); i++)
+        {
+            City next_stop = graph.getCity(c.names.get(i));
+            if(next_stop.warehouse.order.size() > 0 && next_stop.warehouse.peek() < truck.capacity)
+            {
+                distance_travelled += distances.get(i);
+                this.current_city = next_stop;
+            }
+        }
+    }
 }
